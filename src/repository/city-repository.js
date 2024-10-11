3f3efc8f4f1d6de12416bbaaf56e9e3d503d9f9d
Akshatkm1204 <akshatkumar120304@gmail.com>
2024-10-11 07:45:22 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,23 +1,54 @@
 const { City } = require('../models/index');
 
 class CityRepository {
-    async createCity({ name }) {
+    
+
+    async createCity({ name }){
         try {
-            const city = await City.create ( { name });
-            return city;
-        }catch(error){
+            const city = await City.create({
+                name
+            });
+        } catch (error) {
+            console.log("something went wrong in the repository layer.");
+            throw{ error };
+            
+        }
+    }
+
+    async deleteCity({ cityId  }){
+        try {
+            await City.destroy({
+                where: {
+                    id: cityId
+                }
+            });
+            return true;
+        } catch (error) {
+            console.log("something went wrong in the repository layer");
+            throw{ error };
+        }
+    }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await City.update(data, {
+                where : {
+                    id: cityId 
+                }
+            });
+            return city; 
+        } catch (error) {
+            console.log("something went wrong in the repository layer.");
             throw{error};
         }
     }
 
-    async deleteCity(cityid) {
+    async getCity(cityId){
         try {
-            await City.destroy({
-                where: {
-                    id: cityid
-                }
-            });
-        }catch(error){
+            const city = await City.findByPk(cityId);
+            return City; 
+        } catch (error) {
+            console.log("something went wrong in the repository layer");
             throw{error};
         }
     }
